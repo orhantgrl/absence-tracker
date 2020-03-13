0e4baf77997cb12aa47da8f0a0fd4b8485792b38
Orhan Tugrul <orhan.tugrul.61@gmail.com>
2020-03-13 15:19:33 +0300

    Source code refactored and add shared preferences

--- app/src/main/java/com/theloocale/absencetracker/activity/LoginActivity.java
+++ app/src/main/java/com/theloocale/absencetracker/activity/LoginActivity.java
@@ -1,5 +1,7 @@
 package com.theloocale.absencetracker.activity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -11,6 +13,11 @@ import com.theloocale.absencetracker.R;
 import com.theloocale.absencetracker.databinding.ActivityLoginBinding;
 
 import java.util.Objects;
+
+/**
+ * @author orhangrl
+ * created on 3/12/2020.
+ */
 
 public class LoginActivity extends AppCompatActivity {
     private ActivityLoginBinding binding;
@@ -25,10 +32,23 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(view);
 
         binding.loginLetsGoButton.setOnClickListener((v) -> {
-            String loginInput = Objects.requireNonNull(binding.loginNameInput.getText()).toString();
+            String loginInput = Objects.requireNonNull(binding.loginNameInput.getText())
+                    .toString()
+                    .trim();
+
             if (loginInput.isEmpty() || loginInput.length() < 3) {
                 binding.loginNameInputLayout.setError(getResources().getString(
                         R.string.login_error_content));
+            } else {
+                SharedPreferences.Editor editor = getSharedPreferences(
+                        getString(R.string.global_preferences_name), MODE_PRIVATE).edit();
+
+                editor.putString("userName", loginInput);
+                editor.putBoolean("isLogged", true);
+                editor.apply();
+
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
             }
         });
 
